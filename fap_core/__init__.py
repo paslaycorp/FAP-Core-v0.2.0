99e295f52ac034fdc0bd7579480ced5e43207e1b
Rick Paslay <304829193+paslaycorp@users.noreply.github.com>
2026-09-20 04:24:50 -0500

    maintenance: correct package author metadata

--- fap_core/__init__.py
+++ fap_core/__init__.py
@@ -1,6 +1,6 @@
 """FAP-Core v0.2.0"""
 __version__ = "0.2.0"
-__author__ = "Patrick Paslayat"
+__author__ = "Patrick Paslay"
 from .artifact import Artifact, GeoStamp, DeviceStamp, OracleResults, ArtifactStatus
 from .scoring.score import ScoringEngine, ScoreResult, quick_score
 from .verify import VerificationPipeline, verify, verify_async
